37d0add424cede56b428b3a1c11701ba7e28ed0c
Xavery Scott <xavery.williams@gmail.com>
2021-12-06 04:13:45 -0500

    notes removed

--- quote-machine.js
+++ quote-machine.js
@@ -1,21 +1,17 @@
 // This program creates caveman quotes of prophetic nature.
 
-// words to be used 
 let nouns =  "Actor Gold Painting Advertisement Grass Parrot Afternoon Greece Pencil Airport Guitar Piano Ambulance Hair Pillow Animal Hamburger Pizza Answer Helicopter Planet Apple Helmet Plastic Army Holiday Portugal Australia Honey Potato Balloon Horse Queen Banana Hospital Quill Battery House RainBeach Hydrogen Rainbow Beard Ice Raincoat Bed Insect Refrigerator Belgium Insurance Restaurant Boy Iron River Branch Island Rocket Breakfast Jackal RoomBrother Jelly RoseCamera Jewellery Russia Candle Jordan Sandwich Car Juice School Caravan Kangaroo Scooter Carpet King Shampoo Cartoon Kitchen Shoe China Kite Soccer Church Knife Spoon Crayon Lamp Stone Crowd Lawyer Sugar Daughter Leather Sweden Death Library Teacher Denmark Lighter Telephone Diamond Lion Television Dinner Lizard Tent Disease Lock Thailand Doctor London Tomato Dog Lunch Toothbrush Dream Machine Traffic Dress Magazine Train Easter Magician Truck Egg Manchester Uganda Eggplant Market Umbrella Egypt Match Van Elephant Microphone Vase Energy Monkey Vegetable Engine Morning Vulture England Motorcycle Wall Evening Nail Whale Eye Napkin Window Family Needle Wire Finland Nest Xylophone Fish Nigeria Yacht Flag Night YakFlower Notebook  Zebra Football Ocean Zoo Forest Oil Garden Fountain Orange Gas France Oxygen Girl Furniture Oyster Glass Garage Ghost";
 let verbs = "Knew Believed Understand Doubt Think  Suppose Recognize Forget Remember Imagine Mean Agree Disagree Deny Promise Satisfy Realise Appear Astonish Please Impress Surprise Concern Have Own Possess Lack Consist Involve Include Contain Love Like Dislike Hate Adore Prefer Care for Mind Want Need Desire Wish Hope Appreciate Value Cost Measure Weigh Owe Seem Fit Depend Matter";
 let pronouns = "I We You They";
 
-// creates array from the words
 let xpronounsArr = pronouns.split(" ");
 let xnounsArr = nouns.split(" ");
 let xverbsArr = verbs.split(" ");
 
-//contains these arrays of words
 let nounsArr = xnounsArr;
 let pronounsArr = xpronounsArr;
 let verbsArr = xverbsArr;
 
-// randomly chooses an index of each array of words and returns them in sentence format
 function cavepersonquoteMachine () {
   let x = 0;
   let y = 0;
